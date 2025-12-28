ce221e05058278ed2a7fe5f620b70a8b360d7c4d
NikitaSuharev <niita5505@icloud.com>
2025-12-28 11:38:41 +0300

    Read and parse JSON files

--- hexlet_code/scripts/gendiff.py
+++ hexlet_code/scripts/gendiff.py
@@ -1,4 +1,9 @@
 import argparse
+import json
+
+
+def parse_file(filepath):
+    return json.load(open(filepath))
 
 
 def main():
@@ -8,14 +13,19 @@ def main():
 
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-
     parser.add_argument(
         '-f',
         '--format',
         help='set format of output'
     )
 
-    parser.parse_args()
+    args = parser.parse_args()
+
+    data1 = parse_file(args.first_file)
+    data2 = parse_file(args.second_file)
+
+    print(data1)
+    print(data2)
 
 
 if __name__ == '__main__':
